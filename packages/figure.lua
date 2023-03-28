f315c668e6ed1fd0fbe3b1a1fd8e4506d2ec680b
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2023-03-28 16:27:35 -0300

    Margin options for figures

--- packages/figure.lua
+++ packages/figure.lua
@@ -6,8 +6,36 @@ package._name = "figure"
 function package:registerCommands()
   self:registerCommand("fig", function (options, _)
     SU.required(options, "src", "including image file")
-    local width =  SU.cast("measurement", options.width or "100%fw"):tonumber()
-    local height = SU.cast("measurement", options.height or 0):tonumber()
+
+    local widthMeasure = assert(SU.cast("measurement", options.width or "100%fw"))
+    local heightMeasure = assert(SU.cast("measurement", options.height or 0))
+
+    -- Margins
+    -- They should be absolute relating to the frame, so we have to compensate
+    -- for the default document margins (i.e. we are pottentially shrinking/enlarging
+    -- the frame when we choose a different margin, so we have to update what "50%fw"
+    -- represents)
+    local docMarginX = SILE.settings:get("sine.gutter") / 2
+    local docMarginTop = SILE.settings:get("sine.margin-mid") / 2
+    local marginX = SU.cast("measurement", options['margin-x'] or docMarginX)
+    local marginTop = SU.cast("measurement", options['margin-top'] or docMarginTop)
+    -- X movement due to margin
+    local xDelta = (marginX - docMarginX):tonumber()
+    -- Top movement due to margin
+    local topDelta = (marginTop - docMarginTop):tonumber()
+
+    local width = widthMeasure:tonumber()
+    -- Update "%fw" (frame width) measures due to margins
+    if widthMeasure.unit == "%fw" then
+      width = (widthMeasure.amount/100) * (SILE.typesetter.frame:width():tonumber() - xDelta*2)
+    end
+
+    local height = heightMeasure:tonumber()
+    -- Update "%fh" (frame height) measures due to margins
+    if heightMeasure.unit == "%fw" then
+      height = (heightMeasure.amount/100) * (SILE.typesetter.frame:height():tonumber() - topDelta*2)
+    end
+
     local pageno = SU.cast("integer", options.page or 1)
     local src = SILE.resolveFile(options.src) or SU.error("Couldn't find file "..options.src)
     local box_width, box_height, _, _ = SILE.outputter:getImageSize(src, pageno)
@@ -22,12 +50,13 @@ function package:registerCommands()
       end
 
       SILE.typesetter:pushHbox({
-        width= box_width / (sx),
-        height= box_height / (sy),
-        depth= 0,
-        value= src,
+        width = box_width / (sx),
+        height = box_height / (sy) + topDelta, -- Margin-compensated height
+        h = box_height / (sy),
+        depth = 0,
+        value = src,
         outputYourself = function (node, typesetter, _)
-          SILE.outputter:drawImage(node.value, typesetter.frame.state.cursorX + 20, typesetter.frame.state.cursorY-node.height, node.width, node.height, pageno)
+          SILE.outputter:drawImage(node.value, typesetter.frame.state.cursorX+xDelta, typesetter.frame.state.cursorY-node.height+topDelta, node.width, node.h, pageno)
           typesetter.frame:advanceWritingDirection(node.width)
       end})
     end)
